e96b6dc679abb59114e27698dc6ae91d20999c23
trahman14 <tarek.rahman@intmac-q3ulg3qd.internal.pixacore.com>
2018-07-17 15:49:52 -0400

    Thread changed again to 3000 sec

--- src/test/java/stepPack/BMICalculator.java
+++ src/test/java/stepPack/BMICalculator.java
@@ -26,7 +26,7 @@ public class BMICalculator extends AbstractClass{
 	public void i_click_on_calculate_button() throws InterruptedException {
 		
 		driver.findElement(By.id("Calculate")).click();
-		Thread.sleep(6000);
+		Thread.sleep(3000);
 	    
 	}
 
