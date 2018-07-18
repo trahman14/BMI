cf598c0966ece0a212e9cbe0800e7ea718ba402d
trahman14 <tarek.rahman@intmac-q3ulg3qd.internal.pixacore.com>
2018-07-18 11:13:17 -0400

    fixed git url in jenkins

--- src/test/java/stepPack/BMICalculator.java
+++ src/test/java/stepPack/BMICalculator.java
@@ -26,7 +26,7 @@ public class BMICalculator extends AbstractClass{
 	public void i_click_on_calculate_button() throws InterruptedException {
 		
 		driver.findElement(By.id("Calculate")).click();
-		Thread.sleep(7000);
+		Thread.sleep(9000);
 	    
 	}
 
